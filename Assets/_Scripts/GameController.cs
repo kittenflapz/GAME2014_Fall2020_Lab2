2a3df6338def501db8e07abe13fcf7133dc7ce83
Catt Symonds <catherine.symonds.thompson@gmail.com>
2020-09-29 14:02:14 -0400

    Initial commit

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    public TMP_Text LivesLabel;
+    public TMP_Text ScoreLabel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(Input.deviceOrientation)
+        {
+            case DeviceOrientation.Portrait:
+                
+                break;
+            case DeviceOrientation.LandscapeLeft:
+                LivesLabel.rectTransform.anchoredPosition = new Vector2(370.0f, -38.0f);
+                LivesLabel.rectTransform.anchoredPosition = new Vector2(353.0f, -38.0f);
+                break;
+            case DeviceOrientation.LandscapeRight:
+                LivesLabel.rectTransform.anchoredPosition = new Vector2(248.0f, -83.0f);
+                LivesLabel.rectTransform.anchoredPosition = new Vector2(-500.0f, -83.0f);
+                break;
+            case DeviceOrientation.Unknown:
+                
+                break;
+            default:
+                
+                break;
+        }
+        
+    }
+}
---
+++ Assets/_Scripts/TestButtonBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TestButtonBehaviour : MonoBehaviour
+{
+    public TMP_Text SceneLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Event Handler for the StartButton_Pressed Event
+    public void OnTestButtonPressed()
+    {
+        Debug.Log("NextButton Pressed");
+        SceneLabel.text = "test";
+    }
+}
